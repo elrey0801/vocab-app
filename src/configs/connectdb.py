e9ba8ed33b73be920119b26ea82f2cacbf29ab0c
elrey0801 <tungnguyen.dsc@gmail.com>
2023-11-20 17:08:42 +0700

    fix import .env

--- src/configs/connectdb.py
+++ src/configs/connectdb.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '../..', '.env')
+load_dotenv(dotenv_path)
+
 DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+print(DATABASE_URL)
 try:
     engine = create_engine(DATABASE_URL, echo=True)
     print('Connect DB:: OK')
