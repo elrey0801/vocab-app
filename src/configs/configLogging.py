734b8cea86f7f7dd6e38defc46725e4df3445183
elrey0801 <tungnguyen.dsc@gmail.com>
2023-12-14 00:35:36 +0700

    fix backslash on log file directory

--- src/configs/configLogging.py
+++ src/configs/configLogging.py
@@ -6,7 +6,7 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '..\..', '.env')
 load_dotenv(dotenv_path)
 
 def configLogging():
-    logging.basicConfig(filename='..\server.log', level=logging.DEBUG)
+    logging.basicConfig(filename='../server.log', level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     if os.getenv('ENV') == 'production':
         logger.setLevel(logging.INFO)
